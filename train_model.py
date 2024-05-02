144e256a8419535738f2d02f2e4ae1c55073718e
Anant Gupta <anantgupta35@gmail.com>
2024-05-01 22:27:00 -0400

    training code complete

--- train_model.py
+++ train_model.py
@@ -33,19 +33,20 @@ def train_pde(model, tar_sc, dataloader, optimizer, batchsize, device, epochs=10
                 # y_g = y_g.unsqueeze(1)
                 output = torch.zeros(batchsize, 1, requires_grad=True)
                 output = output.to(device)
-                new_output = torch.zeros_like(output)  # Create a new tensor with the same shape and device
-                for i, x_i in enumerate(x_g):
-                    x_i = x_i.detach().clone().to(torch.float32).unsqueeze(0)
-                    new_output[i] = pde(tar_sc, model, x_i)
+                new_output = pde(tar_sc, model, x_g)
+                # new_output = torch.empty_like(output)  # Create a new tensor with the same shape and device
+                # for i, x_i in enumerate(x_g):
+                #     x_i = x_i.detach().clone().to(torch.float32).unsqueeze(0)
+                #     new_output[i] = pde(tar_sc, model, x_i)
                 output = new_output  # Assign the new tensor to output
                 pde_loss = torch.nn.functional.mse_loss(output, y_g.float())
-                loss = pde_loss
-                print("loss:", loss)
+                # loss = pde_loss
+                print("loss:", pde_loss)
                 optimizer.zero_grad()
-                loss.backward()
+                pde_loss.backward()
                 optimizer.step()
             if epoch % 25 == 0:
-                pbar.write(f"Epoch {epoch}: loss={loss.item():.3e}")
+                pbar.write(f"Epoch {epoch}: loss={pde_loss.item():.3e}")
             pbar.update()
     return model
 
@@ -54,11 +55,15 @@ def solve_newton_step_pinn(tar_sc, lr, batchsize, device, epochs=25):
     model = model.to(device)
     dataset_path = "dataset_sin_10000.pth"
     dataset = torch.load(dataset_path)
-    dataloader = DataLoader(dataset, batch_size=batchsize, shuffle=True)
+    train_set, test_set = torch.utils.data.random_split(dataset, [0.7, 0.3])
+    train_dataloader = DataLoader(train_set, batch_size=batchsize, shuffle=True)
+    test_dataloader = DataLoader(test_set, batch_size=batchsize, shuffle=True)
+
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    model = train_pde(model, tar_sc, dataloader, optimizer, batchsize, epochs)
+    model = train_pde(model, tar_sc, train_dataloader, optimizer, batchsize, epochs)
     model.eval()
-    # with torch.no_grad():
-    #     x_gr_tensor = torch.tensor(x_gr, dtype=torch.float32, device=device).unsqueeze(1)
-    #     model_predictions = model(x_gr_tensor).cpu().numpy().flatten()
-    # return model_predictions
+    with torch.no_grad():
+        model_predictions = np.empty(len(test_dataloader) * batchsize)
+        for t, (x_gr, y_gr) in enumerate(test_dataloader):
+            model_predictions = model(x_gr).cpu().numpy().flatten()
+    return model_predictions
